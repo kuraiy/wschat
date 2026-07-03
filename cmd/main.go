5adf8a7e1d0675b8b7590a7a329fd5212b035975
kuraiy <beka1meilun@gmail.com>
2026-07-03 12:16:50 +0000

    refactor: main decomposed

--- cmd/main.go
+++ cmd/main.go
@@ -24,37 +24,23 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	pgPath := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s", os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"),
-		os.Getenv("PGPORT"), os.Getenv("DB_NAME"))
-
-	pool, err := pgxpool.New(context.Background(), pgPath)
+	pool, err := connectToPostgres()
 
 	if err != nil {
 		log.Fatal("Can't connect to DB", err)
 	}
 
 	defer pool.Close()
-	redisAddr := os.Getenv("REDIS")
-	redisPass := os.Getenv("REDISPASS")
 
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:     redisAddr,
-		Password: redisPass,
-		DB:       0,
-	})
+	redisClient := connectToRedis()
 
 	if err := redisClient.Ping(context.Background()).Err(); err != nil {
 		log.Fatal("Can't connect to redis")
 	}
 
-	accessExp, _ := strconv.Atoi(os.Getenv("ACCESS_EXP"))
-	refreshExp, _ := strconv.Atoi(os.Getenv("REFRESH_EXP"))
-	accessSecret := os.Getenv("ACCESS")
-	refreshSecret := os.Getenv("REFRESH")
-
 	rdb := repository.NewRedis(redisClient)
 
-	tm := auth_token.NewManager(accessSecret, refreshSecret, accessExp, refreshExp, *rdb)
+	tm := configureTokenManager(*rdb)
 	ur := repository.New(pool)
 	us := service.New(ur, tm)
 	uh := handler.NewAuth(us, tm)
@@ -66,4 +52,37 @@ func main() {
 		log.Fatal("Server didn't start")
 	}
 
+}
+
+func connectToPostgres() (*pgxpool.Pool, error) {
+	pgPath := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s", os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"),
+		os.Getenv("PGPORT"), os.Getenv("DB_NAME"))
+
+	pool, err := pgxpool.New(context.Background(), pgPath)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func connectToRedis() *redis.Client {
+	redisAddr := os.Getenv("REDIS")
+	redisPass := os.Getenv("REDISPASS")
+
+	return redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPass,
+		DB:       0,
+	})
+}
+
+func configureTokenManager(repo repository.Redis) *auth_token.TokenManager {
+	accessExp, _ := strconv.Atoi(os.Getenv("ACCESS_EXP"))
+	refreshExp, _ := strconv.Atoi(os.Getenv("REFRESH_EXP"))
+	accessSecret := os.Getenv("ACCESS")
+	refreshSecret := os.Getenv("REFRESH")
+
+	return auth_token.NewManager(accessSecret, refreshSecret, accessExp, refreshExp, repo)
 }
