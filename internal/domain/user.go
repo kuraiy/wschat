506df2560ce4419cba20e34e36f791d380631f32
kuraiy <beka1meilun@gmail.com>
2026-07-04 13:26:49 +0000

    add: interface methods

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -14,10 +14,14 @@ type User struct {
 type UserRepository interface {
 	CreateUser(ctx context.Context, username string, password string) error
 	GetUserByUsername(ctx context.Context, username string) (User, error)
+	GetByID(ctx context.Context, id int64) (User, error)
+	ChangeUsername(ctx context.Context, id int64, newUsername string) error
 }
 
 type UserService interface {
 	SignUp(ctx context.Context, username string, password string) error
 	SignIn(ctx context.Context, username string, password string) (dto.LoginOutput, error)
 	SignOut(ctx context.Context, refresh string)
+	ChangeUsername(ctx context.Context, id int64, newUsername string) error
+	GetUser(ctx context.Context, id int64) (dto.GetMeDTO, error)
 }
