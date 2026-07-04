45ab697d9eaaf0874767fe99ea3e1e7a86cd2e9b
kuraiy <beka1meilun@gmail.com>
2026-07-04 13:25:25 +0000

    feat: change username sqlc gen code

--- db/queries/user_queries.sql
+++ db/queries/user_queries.sql
@@ -10,3 +10,21 @@ RETURNING id, username;
 SELECT id, username, password_hash FROM users
 WHERE username = $1
 LIMIT 1;
+
+
+-- name: UpdateUsername :one
+UPDATE users
+SET username = $2
+WHERE id = $1
+RETURNING id, username;
+
+-- name: UpdatePassword :one
+UPDATE users
+SET password_hash = $2
+WHERE id = $1
+RETURNING id, username;
+
+-- name: GetById :one
+SELECT id, username, password_hash FROM users
+WHERE id = $1
+LIMIT 1;
