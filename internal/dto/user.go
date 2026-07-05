90d14fecafc5e9e36937b615412d44263abb87df
kuraiy <beka1meilun@gmail.com>
2026-07-05 13:08:09 +0000

    password endpoing dto

--- internal/dto/user.go
+++ internal/dto/user.go
@@ -4,6 +4,11 @@ type ChangeUsernameDTO struct {
 	Username string `json:"username" binding:"required,username"`
 }
 
+type ChangePasswordDTO struct {
+	OldPassword string `json:"old_password" binding:"required"`
+	NewPassword string `json:"new_password" binding:"required,password"`
+}
+
 type GetMeDTO struct {
 	ID       int64
 	Username string
